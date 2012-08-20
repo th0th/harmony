fe1e9091a7bf5f0a3e5f308fcf7f4d2d6515b545
th0th <th0th@returnfalse.net>
2012-08-21 02:47:35 +0300

    add taghandler

---
+++ harmony/taghandler.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from mutagen.easyid3 import EasyID3
+
+class TagHandler:
+	def __init__(self):
+		self.mask = None
+
+	def set_mask(self, mask):
+		self.mask = mask
+
+	def read_tags(self, filepath):
+		tags = EasyID3(filepath)
+
+		new_tags = {}
+
+		print tags
+
+		for tag in tags:
+			new_tags[tag] = tags[tag][0]
+
+		return new_tags
+
+	def generate_filename(self, filepath):
+		filename = self.mask.format(**self.read_tags(filepath))
+
+		return filename
